a87f7425edaa90f1a1e4c8c724008f3c26acacaa
Yang <yangah9991@gmail.com>
2022-10-29 15:20:27 +0800

    getting the normal of the sphere

--- Sandbox/src/Application.cpp
+++ Sandbox/src/Application.cpp
@@ -4,40 +4,48 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
+double hit_sphere(const point3& center, double radius, const ray& r) {
+	const vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(oc, r.direction());
+	double c = dot(oc, oc) - radius * radius;
+	const double discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		return -1.0;
+	} else {
+		return (-b - sqrt(discriminant) ) / (2.0*a);
+	}
 }
 
 color ray_color(const ray& r) {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		return color(1, 0, 0);
+	double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+		return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5*(unit_direction.y() + 1.0); 
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
 	// Image
-	const auto aspect_ratio = 16.0 / 9.0;
+	const double aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	// Camera
 
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
 
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	point3 origin = point3(0, 0, 0);
+	vec3 horizontal = vec3(viewport_width, 0, 0);
+	vec3 vertical = vec3(0, viewport_height, 0);
+	vec3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
 	// Render
 
@@ -46,8 +54,8 @@ int main() {
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
+			double u = double(i) / (image_width - 1);
+			double v = double(j) / (image_height - 1);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
